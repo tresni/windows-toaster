9095b5e2c7ed4c8c2dee02559bfcb56801625ec7
tresni <tresni@223d167a-fe68-11de-9adb-2bc960bfb704>
2010-01-11 04:22:45 +0000

    Initial Import

---
+++ trunk/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Toaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.pctIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblTitle.Location = new System.Drawing.Point(109, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.lblTitle.Click += new System.EventHandler(this.ClickToClose);
+            this.lblTitle.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            // 
+            // lblText
+            // 
+            this.lblText.Location = new System.Drawing.Point(12, 35);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(116, 66);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Text";
+            this.lblText.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.lblText.Click += new System.EventHandler(this.ClickToClose);
+            this.lblText.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            // 
+            // pctIcon
+            // 
+            this.pctIcon.Location = new System.Drawing.Point(0, 0);
+            this.pctIcon.Name = "pctIcon";
+            this.pctIcon.Size = new System.Drawing.Size(32, 32);
+            this.pctIcon.TabIndex = 2;
+            this.pctIcon.TabStop = false;
+            this.pctIcon.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.pctIcon.Click += new System.EventHandler(this.ClickToClose);
+            this.pctIcon.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(136, 106);
+            this.ControlBox = false;
+            this.Controls.Add(this.pctIcon);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.Click += new System.EventHandler(this.ClickToClose);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pctIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.PictureBox pctIcon;
+    }
+}
---
+++ trunk/Toaster.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text;
+using Growl.Connector;
+using Growl.CoreLibrary;
+using Snarl;
+
+
+namespace Toaster
+{
+    public class Toaster
+    {
+        Mutex ToastMutex = new Mutex(false, "Microsoft.Messenger.ToastSemaphore");
+        GrowlConnector growlConnector = new GrowlConnector();
+        System.Drawing.Image icon = null;
+
+        string appName = "";
+        bool registered = false;
+        public bool Registered { get { return registered; } }
+        public bool UseCustom = true;
+
+        public enum ToasterType {
+            TOASTER_NONE,
+            TOASTER_GROWL,
+            TOASTER_SNARL,
+            TOASTER_CUSTOM
+        };
+
+        public Toaster(string AppName)
+        {
+            this.appName = AppName;
+            growlConnector.EncryptionAlgorithm = Cryptography.SymmetricAlgorithmType.AES;
+        }
+
+        public Toaster(string AppName, System.Drawing.Image icon)
+            : this(AppName)
+        {
+            this.icon = icon;
+        }
+
+        public Toaster(string AppName, string password)
+            : this(AppName)
+        {
+            growlConnector.Password = password;
+        }
+
+        public Toaster(string AppName, string Password, System.Drawing.Image icon)
+            : this(AppName, Password)
+        {
+            this.icon = icon;
+        }
+
+        public bool IsToasterWorking() {
+            return (growlConnector.IsGrowlRunning() || SnarlConnector.GetSnarlWindow() != IntPtr.Zero || this.UseCustom);
+        }
+
+        public ToasterType WhichToaster() {
+            if (growlConnector.IsGrowlRunning())
+                return ToasterType.TOASTER_GROWL;
+            if (SnarlConnector.GetSnarlWindow() != IntPtr.Zero)
+                return ToasterType.TOASTER_SNARL;
+            if (this.UseCustom)
+                return ToasterType.TOASTER_CUSTOM;
+            return ToasterType.TOASTER_NONE;
+        }
+
+        public bool Register(ToasterNotificationType[] notifications)
+        {
+            if (!this.IsToasterWorking()) return false;
+            this.registered = true;
+            switch (this.WhichToaster())
+            {
+                case ToasterType.TOASTER_SNARL:
+                    M_RESULT res = SnarlConnector.RegisterConfig(IntPtr.Zero, this.appName, 0);
+
+                    foreach (ToasterNotificationType notification in notifications)
+                    {
+                        SnarlConnector.RegisterAlert(this.appName, notification.ToString());
+                    }
+                    return res == M_RESULT.M_OK;
+                case ToasterType.TOASTER_GROWL:
+                    growlConnector.Register(new Growl.Connector.Application(this.appName), (NotificationType[])notifications);
+                    return true;
+                case ToasterType.TOASTER_CUSTOM:
+                    return true;
+                default:
+                    this.registered = false;
+                    return false;
+            }
+        }
+
+        public long Toast(string title, string text, string type)
+        {
+            if (!this.IsToasterWorking() || !this.registered) return 0;
+            long id;
+            switch (this.WhichToaster())
+            {
+                case ToasterType.TOASTER_GROWL:
+                    id = DateTime.Now.Ticks;
+                    growlConnector.Notify(new Notification(this.appName, type, id.ToString(), title, text));
+                    return id;
+                case ToasterType.TOASTER_SNARL:
+                    id = SnarlConnector.ShowMessageEx(type, title, text, 0, "", IntPtr.Zero, 0, "");
+                    return id;
+                case ToasterType.TOASTER_CUSTOM:
+                    bool hasMutex = ToastMutex.WaitOne(new TimeSpan(0, 0, 60));
+                    new Form1(title, text, (System.Drawing.Image)icon).Show();
+                    if (hasMutex)
+                        ToastMutex.ReleaseMutex();
+                    return 0;
+                default:
+                    return 0;
+            }
+        }
+    }
+
+    public class ToasterNotificationType : NotificationType
+    {
+        public ToasterNotificationType(string name) : base(name) { }
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
